2fac334153e0959bbd43da14a4dc34341be06fc3
YunYouJun <me@yunyoujun.cn>
2020-07-09 18:03:22 +0800

    :art: simplify answer structure code

--- src/plugins/answer.ts
+++ src/plugins/answer.ts
@@ -6,10 +6,20 @@ import axios from "axios";
 import { renderString } from "@utils/message";
 
 interface AnswerConfig extends Config.Match {
-  listen: Config.Listen;
+  /**
+   * 监听
+   */
+  listen: string | Config.Listen;
+  /**
+   * 不监听
+   */
+  unlisten?: Config.Listen;
+  /**
+   * API 地址，存在时，自动渲染字符串
+   */
   api?: string;
   reply: string | MessageType.MessageChain;
-  quote: boolean;
+  quote?: boolean;
   else?: string | MessageType.MessageChain;
 }
 
@@ -19,19 +29,15 @@ interface AnswerConfig extends Config.Match {
  * @param content 
  */
 async function renderStringByApi(api: string, content: string | MessageType.MessageChain) {
-  if (api) {
-    const { data } = await axios.get(api);
-    if (typeof content === "string") {
-      return renderString(content, data, "data");
-    } else {
-      (content as any).forEach((msg: MessageType.SingleMessage) => {
-        if (msg.type === "Plain") {
-          msg.text = renderString(msg.text, data, "data");
-        }
-      });
-      return content;
-    }
+  const { data } = await axios.get(api);
+  if (typeof content === "string") {
+    return renderString(content, data, "data");
   } else {
+    (content as any).forEach((msg: MessageType.SingleMessage) => {
+      if (msg.type === "Plain") {
+        msg.text = renderString(msg.text, data, "data");
+      }
+    });
     return content;
   }
 }
@@ -40,33 +46,34 @@ export default function answer(ctx: ElBot) {
   const config = ctx.el.config;
   const mirai = ctx.mirai;
 
-  mirai.on('message', (msg: MessageType.SingleMessage) => {
+  mirai.on('message', async (msg: MessageType.SingleMessage) => {
     if (config.answer) {
-      config.answer.every(async (ans: AnswerConfig) => {
-        // 默认监听所有
 
-        if (msg.plain) {
+      // use async in some
+      // https://advancedweb.hu/how-to-use-async-functions-with-array-some-and-every-in-javascript/
+      let ans: AnswerConfig = {
+        listen: 'all',
+        reply: '喵喵喵？'
+      };
+
+      for await (ans of config.answer) {
+        let replyContent = null;
+        if (msg.plain && match(msg.plain, ans)) {
+          // 默认监听所有
           if (isListening(msg.sender, ans.listen || "all")) {
-            if (ans.reply) {
-              if (match(msg.plain, ans)) {
-                if (ans.api) ans.reply = await renderStringByApi(ans.api, ans.reply);
-                msg.reply(ans.reply, ans.quote);
-                return false;
-              }
-            }
-          } else {
-            if (ans.else) {
-              if (match(msg.plain, ans)) {
-                if (ans.api) ans.else = await renderStringByApi(ans.api, ans.else);
-                msg.reply(ans.else, ans.quote);
-                return false;
-              }
-            }
+            replyContent = ans.api ? await renderStringByApi(ans.api, ans.reply) : ans.reply;
+          } else if (ans.else) {
+            // 后续可以考虑用监听白名单、黑名单优化
+            replyContent = ans.api ? renderStringByApi(ans.api, ans.else) : ans.else;
+          }
+
+          if (replyContent) {
+            msg.reply(replyContent, ans.quote);
+            // 有一个满足即跳出
+            break;
           }
         }
-
-        return true;
-      });
+      }
     }
   });
 }
--- src/utils/message.ts
+++ src/utils/message.ts
@@ -6,41 +6,46 @@ import { Config } from "mirai-ts";
  * 是否监听发送者
  * @param {Object} sender
  */
-function isListening(sender: MessageType.Sender, listen: Config.Listen) {
+function isListening(sender: MessageType.Sender, listen: string | Config.Listen) {
   const config = el.config;
 
-  // 监听所有
-  if (listen === "all") {
-    return true;
-  }
+  if (typeof listen === 'string') {
 
-  // 监听 master
-  if (listen === "master" && config.master.includes(sender.id)) {
-    return true;
-  }
-
-  // 监听管理员
-  if (listen === "admin" && config.admin.includes(sender.id)) {
-    return true;
-  }
-
-  // 指定 QQ
-  if (listen.friend && listen.friend.includes(sender.id)) {
-    return true;
-  }
-
-  if (sender.group) {
-    // 群
-    if (
-      listen === "group" ||
-      (listen.group && listen.group.includes(sender.group.id))
-    ) {
+    // 监听所有
+    if (listen === "all") {
       return true;
     }
-  } else {
-    // 私聊时，判断是否监听 friend（其余在上方指定 QQ 时已判断过）
-    if (listen === "friend") {
+
+    // 监听 master
+    if (listen === "master" && config.master.includes(sender.id)) {
       return true;
+    }
+
+    // 监听管理员
+    if (listen === "admin" && config.admin.includes(sender.id)) {
+      return true;
+    }
+
+  } else {
+
+    // 指定 QQ
+    if (listen.friend && listen.friend.includes(sender.id)) {
+      return true;
+    }
+
+    if (sender.group) {
+      // 群
+      if (
+        listen === "group" ||
+        (listen.group && listen.group.includes(sender.group.id))
+      ) {
+        return true;
+      }
+    } else {
+      // 私聊时，判断是否监听 friend（其余在上方指定 QQ 时已判断过）
+      if (listen === "friend") {
+        return true;
+      }
     }
   }
 
