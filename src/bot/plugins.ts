012dddcd49f3a66acbe6a436fd6cba7cb401fd0d
YunYouJun <me@yunyoujun.cn>
2020-07-26 15:28:15 +0800

    :bug: fix module.exports without default

--- src/bot/plugins.ts
+++ src/bot/plugins.ts
@@ -1,32 +1,32 @@
-import log from 'mirai-ts/dist/utils/log'
-import Bot from '.'
+import log from 'mirai-ts/dist/utils/log';
+import Bot from '.';
 
 export interface Plugin {
-  name: string
-  version: string
-  description: string
+  name: string;
+  version: string;
+  description: string;
 }
 
-export type PluginType = 'default' | 'official' | 'community' | 'custom'
+export type PluginType = 'default' | 'official' | 'community' | 'custom';
 
 const PluginTypeMap = {
   default: '默认插件',
   official: '官方插件',
   community: '社区插件',
   custom: '自定义插件',
-}
+};
 
 export default class Plugins {
-  default: Plugin[]
-  official: Plugin[]
-  community: Plugin[]
-  custom: Plugin[]
+  default: Plugin[];
+  official: Plugin[];
+  community: Plugin[];
+  custom: Plugin[];
   constructor(public bot: Bot) {
-    const plugins = bot.el.config.plugins
-    this.default = plugins.default || []
-    this.official = plugins.official || []
-    this.community = plugins.community || []
-    this.custom = plugins.custom || []
+    const plugins = bot.el.config.plugins;
+    this.default = plugins.default || [];
+    this.official = plugins.official || [];
+    this.community = plugins.community || [];
+    this.custom = plugins.custom || [];
   }
 
   /**
@@ -35,40 +35,41 @@ export default class Plugins {
    * @param path 所在路径
    */
   load(type: PluginType) {
-    const config = this.bot.el.config
+    const config = this.bot.el.config;
     if (config.plugins[type]) {
       config.plugins[type].forEach((name: string) => {
-        let pkgName = name
+        let pkgName = name;
 
         switch (type) {
           case 'default':
-            pkgName = `../plugins/${name}`
-            break
+            pkgName = `../plugins/${name}`;
+            break;
           case 'official':
-            pkgName = `@el-bot/plugin-${name}`
-            break
+            pkgName = `@el-bot/plugin-${name}`;
+            break;
           case 'community':
-            pkgName = `el-bot-plugin-${name}`
-            break
+            pkgName = `el-bot-plugin-${name}`;
+            break;
           case 'custom':
-            pkgName = name
-            break
+            pkgName = name;
+            break;
           default:
-            break
+            break;
         }
 
         try {
-          const plugin = require(pkgName).default
+          // 适配 module.exports
+          const plugin = require(pkgName).default || require(pkgName);
 
           let pkg = {
             version: '未知',
             description: '未知',
-          }
+          };
           try {
-            pkg = require(`${pkgName}/package.json`)
+            pkg = require(`${pkgName}/package.json`);
           } catch {
             if (type !== 'default') {
-              log.warning(`${name} 插件没有相关描述信息`)
+              log.warning(`${name} 插件没有相关描述信息`);
             }
           }
 
@@ -77,14 +78,14 @@ export default class Plugins {
               name,
               version: plugin.version || pkg.version,
               description: plugin.description || pkg.description,
-            })
-            this.use(name, plugin)
+            });
+            this.use(name, plugin);
           }
         } catch (error) {
-          console.log(error)
-          log.error(`插件 ${name} 加载失败`)
+          console.log(error);
+          log.error(`插件 ${name} 加载失败`);
         }
-      })
+      });
     }
   }
 
@@ -95,17 +96,17 @@ export default class Plugins {
    */
   use(name: string, plugin: Function) {
     if (this.bot.el.config[name]) {
-      plugin(this.bot, this.bot.el.config[name])
+      plugin(this.bot, this.bot.el.config[name]);
     } else {
-      plugin(this.bot)
+      plugin(this.bot);
     }
   }
 
   list(type: PluginType) {
-    let content = PluginTypeMap[type] + ':\n'
+    let content = PluginTypeMap[type] + ':\n';
     this[type].forEach((plugin: Plugin) => {
-      content += `- ${plugin.name}@${plugin.version}: ${plugin.description}\n`
-    })
-    return content
+      content += `- ${plugin.name}@${plugin.version}: ${plugin.description}\n`;
+    });
+    return content;
   }
 }
