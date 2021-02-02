d51b73f71cd24a6dc66da0ced56898e512684ec5
YunYouJun <me@yunyoujun.cn>
2021-02-03 01:26:59 +0800

    :bug: search-image: judge results.length, fix #14

--- src/plugins/search-image/index.ts
+++ src/plugins/search-image/index.ts
@@ -15,6 +15,7 @@ interface SearchImageOptions {
  * @param result 格式化结果
  */
 function formatResult(result: sagiri.SagiriResult): MessageType.MessageChain {
+  if (!result) return [];
   const msgChain = [
     Message.Plain(`\n------------------\n`),
     Message.Image(null, result.thumbnail),
@@ -55,12 +56,19 @@ export default async function searchImage(
 
           try {
             const results = await client(singleMessage.url);
-            const length = options.options?.results || 3;
-            replyContent.push(Message.Plain(`返回 ${length} 个结果`));
-            for (let i = 0; i < length; i++) {
-              const result = results[i];
-              const formatContent = formatResult(result);
-              replyContent = replyContent.concat(formatContent);
+            if (results.length === 0) {
+              replyContent.push(Message.Plain("未搜索到相关图片"));
+            } else {
+              const length = Math.min(
+                options.options?.results || 3,
+                results.length
+              );
+              replyContent.push(Message.Plain(`返回 ${length} 个结果`));
+              for (let i = 0; i < length; i++) {
+                const result = results[i];
+                const formatContent = formatResult(result);
+                replyContent = replyContent.concat(formatContent);
+              }
             }
             msg.reply(replyContent);
 
